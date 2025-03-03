7a98c34cfdb7abd871cc1db2ebbace98482abe4c
Carolina <carolinamitchell@outlook.co.nz>
2025-03-04 06:00:57 +1300

    Add VolcanoService and NetworkService functions within the GeoNet class

--- src/GeoNet.ts
+++ src/GeoNet.ts
@@ -6,15 +6,24 @@ import { StrongRequest, StrongResponse } from "../@types/strong";
 
 import { NewsService } from "./services/NewsService";
 import { NewsFeedRequest, NewsFeedResponse } from "../@types/news";
+
 import { QuakeService } from "./services/QuakeService";
 import { QuakeHistoryResponse, QuakeRequest, QuakeResponse, QuakesRequest, QuakesResponse, QuakeStatsResponse } from "../@types/quake";
+
+import { VolcanoService } from "./services/VolcanoService";
+import { VolcanoAlertLevelResponse, VolcanoQuakeRequest, VolcanoQuakeResponse } from "../@types/volcano";
+
+import { NetworkService } from "./services/NetworkService";
+import { NetworkFDSNStationRequest, NetworkFDSNStationResponse, NetworkGNSStationRequest, NetworkGNSStationResponse, NetworkSensorRequest, NetworkSensorResponse } from "../@types/network";
 
 export class GeoNet {
     constructor(
         private readonly intensityService: IntensityService,
         private readonly strongService: StrongService,
         private readonly newsService: NewsService,
-        private readonly quakeService: QuakeService
+        private readonly quakeService: QuakeService,
+        private readonly volcanoService: VolcanoService,
+        private readonly networkService: NetworkService
     ) {}
 
     public async getIntensity(req: IntensityRequestUnion): Promise<IntensityResponse> {
@@ -43,5 +52,25 @@ export class GeoNet {
 
     public async getQuakes(req: QuakesRequest): Promise<QuakesResponse> {
         return await this.quakeService.getQuakes(req);
+    }
+
+    public async getVolcanoAlertLevel(): Promise<VolcanoAlertLevelResponse> {
+        return await this.volcanoService.getVolcanoAlertLevel();
+    }
+
+    public async getVolcanoQuakes(req: VolcanoQuakeRequest): Promise<VolcanoQuakeResponse> {
+        return await this.volcanoService.getVolcanoQuakes(req);
+    }
+
+    public async getNetworkSensor(req: NetworkSensorRequest): Promise<NetworkSensorResponse> {
+        return await this.networkService.getNetworkSensor(req);
+    }
+
+    public async getNetworkGNSSDetails(req: NetworkGNSStationRequest): Promise<NetworkGNSStationResponse> {
+        return await this.networkService.getNetworkGNSSDetails(req);
+    }
+
+    public async getNetworkFDSNDetails(req: NetworkFDSNStationRequest): Promise<NetworkFDSNStationResponse> {
+        return await this.networkService.getNetworkFDSNDetails(req);
     }
 }
